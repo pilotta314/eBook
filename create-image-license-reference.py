6f8f1238f877bbe098b8149d3becb218c12acc10
Axel Klinger <axel.klinger@web.de>
2019-10-27 09:12:38 +0100

    Create tullu info

--- create-image-license-reference.py
+++ create-image-license-reference.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-#import yaml
-#import json
+import json
 import requests
 import re
 import sys
@@ -16,66 +15,37 @@ for treffer in images:
     if "wikimedia" in link:
         print("TREFFER : " + description + " " + link)
         image_name = re.findall("\/([^\/]*)$", link)[0]
-        # print(image_name)
 
         # get image_name metadata
         session = requests.Session()
         api_url = "https://en.wikipedia.org/w/api.php"
-        title = "File:" + image_name
         params_image = {
             "action": "query",
             "format": "json",
-            "prop": "imageinfo",
-            "titles": "File:" + image_name
-        }
-        params_rights = {
-            "action": "query",
-            "format": "json",
-            "meta" : "siteinfo",
-            "siprop" : "rightsinfo",
+            "prop" : "imageinfo",
+            "iiprop": "user|userid|canonicaltitle|url|extmetadata",
             "titles": "File:" + image_name
         }
 
         image_data = session.get(url=api_url, params=params_image).json()
-        rights_data = session.get(url=api_url, params=params_rights).json()
-        # print(image_data)
-        # print(rights_data)
+        # print(json.dumps(image_data, indent=4, ensure_ascii=0))
 
         IPAGES = image_data["query"]["pages"]
-        rights = rights_data["query"]["rightsinfo"]
-        # print(IPAGES)
-        # print(rights)
 
         for k, v in IPAGES.items():
-            print("ERGEBNIS : " + v["title"] + " is uploaded by User:" + v["imageinfo"][0]["user"] + " under "  + rights["text"])
+            print("LIZENZ : " + v["imageinfo"][0]["extmetadata"]["LicenseShortName"]["value"])
+            image_info = v["imageinfo"][0]
+            image_title = image_info["extmetadata"]["ObjectName"]["value"]
+            image_author = image_info["user"]
+            image_page = image_info["descriptionurl"]
+            license_name = image_info["extmetadata"]["UsageTerms"]["value"]
+            license_short_name = image_info["extmetadata"]["LicenseShortName"]["value"]
+            license_url = image_info["extmetadata"]["LicenseUrl"]["value"]
+            tullu = ("\"" + image_title + "\" von " + image_author + " unter ["  + license_short_name + "](" + license_url + ") auf [Wikimedia Commons](" + image_page + ")")
+            # print(tullu)
+            text = re.sub("!\[" + description + "\]\(" + link + "\)", "![" + description + "](" + link + ")" + "  \n" + tullu, text)
 
-
+course_tagged = open("course-tagged.md", "w")
+course_tagged.write(text)
+course_tagged.close()
 course.close()
-
-
-
-
-# with open("course.md", 'r') as stream:
-    # try:
-    #     data = yaml.safe_load(stream)
-    # except yaml.YAMLError as exc:
-    #     print(exc)
-
-    # data['@context'] = 'http://schema.org/'
-    # data['publisher']['@type'] = 'Person'
-    # data['creator']['@type'] = 'Person'
-
-    # with open("metadata.json", 'w', encoding='utf8') as outputfile:
-    #     jsonstring = json.dumps(data, indent=4, ensure_ascii=0)
-    #     tagstring = '<link rel="license" href="' + data['license'] + '"/>'
-    #     tagstring = tagstring + '<script type="application/ld+json">' + jsonstring + '</script>'
-    #     outputfile.write(tagstring)
-
-    # with open('title.txt', 'w', encoding='utf8') as titlefile:
-    #     titlecontent = '---\n'
-    #     titlecontent = titlecontent + 'title: ' + data['name'] + '\n'
-    #     titlecontent = titlecontent + 'author: ' + data['creator']['givenName'] + ' ' + data['creator']['familyName'] + '\n'
-    #     titlecontent = titlecontent + 'rights: ' + data['license'] + '\n'
-    #     titlecontent = titlecontent + 'language: ' + data['inLanguage'] + '\n'
-    #     titlecontent = titlecontent + '...\n'
-    #     titlefile.write(titlecontent)
